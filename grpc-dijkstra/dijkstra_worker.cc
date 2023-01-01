8c271f190c2895bbdb37d82943b47c02ccd039d7
wladekpal <wladek.palucki@gmail.com>
2023-01-01 20:31:57 +0100

    start of grpc dijkstra

--- grpc-dijkstra/dijkstra_worker.cc
+++ grpc-dijkstra/dijkstra_worker.cc
@@ -4,56 +4,126 @@
 #include <string>
 #include <queue>
 #include <thread>
+#include <mutex>
 #include <grpcpp/grpcpp.h>
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/health_check_service_interface.h>
+#include <grpcpp/server.h>
+#include <grpcpp/server_builder.h>
+#include <grpcpp/server_context.h>
+#include <grpc/grpc.h>
 #include "dijkstra.grpc.pb.h"
 
-using dijkstra::EdgeLen;
-using dijkstra::LenQuery;
-using dijkstra::LenReply;
+using namespace dijkstra;
 using grpc::Channel;
 using grpc::ClientContext;
 using grpc::Status;
+using grpc::Server;
+using grpc::ServerReader;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
 
-class DijkstraWorker
+class DijkstraWorker 
 {
 public:
-    DijkstraWorker(std::shared_ptr<Channel> channel)
-        : stub_(DijkstraMainWorker::NewStub(channel))
+    DijkstraWorker(std::shared_ptr<Channel> channel, 
+                   std::shared_ptr<std::mutex> queue_mut, 
+                   std::shared_ptr<std::priority_queue<int>> dij_queue,
+                   std::string main_address)
+                : stub_(DijkstraMainWorker::NewStub(channel)), queue_mut(queue_mut), dij_queue(dij_queue)
     {
-        SectorReply sector = stub_->hello_and_get_sector(address));
-        this.sector_num = sector;
+        
+        //SectorReply sector = stub_->hello_and_get_sector(address));
+        //sector_num = sector;
     }
+
+
     void run(std::string address)
     {
+        
         ClientContext context;
         Job job;
         std::unique_ptr<ClientReader<Job>> job_reader(stub_->job_stream(&context));
         while(job_reader->Read(&job, address))
         {
             if job.type_of_job == 0 {
-                this.run_dijkstra(job.start_edge, job.end_edge, job.end_edge_region);
+                this.start_dijkstra(job.start_edge, job.end_edge, job.end_edge_region);
             }
         }
     }
-    void run_dijkstra(int start_edge, int end_edge, int ind_edge_region){
+    void start_dijkstra(int start_edge, int end_edge, int ind_edge_region){
 
     }
 
 private:
-    std::unique_ptr<EdgeLen::Stub> stub_;
+    std::unique_ptr<DijkstraMainWorker::Stub> stub_;
     int32_t sector_num;
-    priority_queue<>
+    std::shared_ptr<std::mutex> queue_mut;
+    std::shared_ptr<std::priority_queue<int>> dij_queue;
+};
+
+
+
+class DijkstraWorkerServer final: public DijkstraWorkerService::Service {
+public:
+    DijkstraWorkerServer(std::shared_ptr<std::mutex> queue_mut, 
+                        std::shared_ptr<std::priority_queue<int>> dij_queue,
+                        std::string addr) {
+        this->queue_mut = queue_mut;
+        this->dij_queue = dij_queue;
+        this->addr = addr;
+    }
+
+    Status new_region_entry(ServerContext* context, ServerReader<Entry>* read_stream, JobEnd* reply) {
+        Entry entry;
+        while(read_stream->Read(&entry)){
+            
+        }
+    }
+    
+    void RunServer() {
+        std::string server_address(this->addr);
+
+
+        grpc::EnableDefaultHealthCheckService(true);
+        grpc::reflection::InitProtoReflectionServerBuilderPlugin();
+        ServerBuilder builder;
+        // Listen on the given address without any authentication mechanism.
+        builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+        // Register "service" as the instance through which we'll communicate with
+        // clients. In this case it corresponds to an *synchronous* service.
+        builder.RegisterService(this);
+        // Finally assemble the server.
+        std::unique_ptr<Server> server(builder.BuildAndStart());
+        std::cout << "Server listening on " << server_address << std::endl;
+
+        // Wait for the server to shutdown. Note that some other thread must be
+        // responsible for shutting down the server for this call to ever return.
+        server->Wait();
+    }
+
+private:
+    std::shared_ptr<std::mutex> queue_mut;
+    std::shared_ptr<std::priority_queue<int>> dij_queue;
+    std::string addr;
 };
 
 int main(int argc, char **argv)
 {
     std::string main_server_location;
     main_server_location = "localhost:50051";
-
     std::string my_location{argv[1]};
-    DijkstraClient client(
-        grpc::CreateChannel(main_server_location, grpc::InsecureChannelCredentials()));
-
-    std::thread client_thread(client.run, my_location);
+    std::shared_ptr<std::mutex> queue_mut{};
+    std::shared_ptr<std::priority_queue<int>> dij_queue{};
+    
+    DijkstraWorker worker_client(
+        grpc::CreateChannel(main_server_location, grpc::InsecureChannelCredentials()), queue_mut, dij_queue, main_server_location);
+    
+    DijkstraWorkerServer worker_server(queue_mut, dij_queue, my_location);
+    
+    std::thread client_thread(&DijkstraWorker::run, worker_client);
+    worker_server.RunServer();
+        
     return 0;
 }
